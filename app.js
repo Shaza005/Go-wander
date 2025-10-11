3032ee0581b481258f44fdf2c139e509c30c3fc8
Shaza Zahir <shazazahir123@gmail.com>
2025-10-11 15:26:31 +0530

    add project files

---
+++ app.js
@@ -0,0 +1,79 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const methodOverride=require("method-override");
+const engine=require("ejs-mate");
+const ExpressError=require("./utils/ExpressError.js");
+const mongoose = require('mongoose');
+const session=require("express-session");
+const flash=require("connect-flash");
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js")
+
+const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
+
+app.engine("ejs", engine);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+main().then((res)=>{console.log("connection successful")}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/GO-WANDER');
+}
+
+const sessionOptions={
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+ 7 * 24 * 60 * 60 * 1000,
+        maxAge:7 * 24 * 60 * 60 * 1000,
+        httpOnly:true
+    }
+};
+
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
+});
+
+//listings
+app.use("/listings",listings)
+
+//reviews
+app.use("/listings/:id/reviews",reviews)
+
+app.all(/.*/, (req,res,next)=>{
+    next(new ExpressError(404,"----Page not found----"));
+});
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="something went wrong"}=err;
+    res.status(statusCode).render("error.ejs",{message});
+});
+
+app.listen(8080,(req,res)=>{
+    console.log("listening to port:8080");
+});
