eab444b1ec67254e9adefc4d50381fbf6dbbe6c3
Shaza Zahir <shazazahir123@gmail.com>
2026-05-23 10:20:09 +0530

    solving errors

--- controllers/users.js
+++ controllers/users.js
@@ -5,29 +5,36 @@ module.exports.renderSignupForm=(req,res)=>{
     res.render("users/signup.ejs");
 };
 
-module.exports.signup=async(req,res,next)=>{
-    try{
-    let {username,email,password}=req.body;
-    const newUser= new User({email,username});
-    const registeredUser=await User.register(newUser,password);
-    console.log(registeredUser);
-    req.login(registeredUser,(err)=>{
-        if(err){
-            return next(err);
-        }
-          req.flash("success","Welcome to GO-WANDER");
-          res.redirect("/listings");
-    })
-  
-    }
-    catch(e){
-        console.log(e);
-        req.flash("error",e.message);
-        res.redirect("/signup");
-        
+module.exports.signup = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+
+        const newUser = new User({
+            email,
+            username
+        });
+
+        const registeredUser = await User.register(newUser, password);
+
+        req.login(registeredUser, (err) => {
+
+            if (err) {
+                console.log("LOGIN ERROR:", err);
+                return res.send(err.message);
+            }
+
+            req.flash("success", "Welcome to GO-WANDER");
+
+            res.redirect("/listings");
+        });
+
+    } catch (e) {
+
+        console.log("SIGNUP ERROR:", e);
+
+        return res.send(e.message);
     }
 };
-
 
 module.exports.renderLoginForm=(req,res)=>{
     res.render("users/login.ejs");
